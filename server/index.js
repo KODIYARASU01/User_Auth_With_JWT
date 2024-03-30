4e6344dc565a68b10524f3388e4f5c2eb677386f
KODIYARASU01 <kodiyarasu02gmail.com>
2024-03-30 07:15:52 +0000

    Server Side mongoDb Conncetion established Sucessfully

--- server/index.js
+++ server/index.js
@@ -3,18 +3,38 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
-
+//Configire DotEnv Configuration :
 dotenv.config();
-
+//App initialized :s
 let app = express();
+//Accept json type data send to server:
+app.use(express.json({ limit: "30mb" }));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
+//Initialize DotEnv Datas :
 let PORT = process.env.PORT || 3000;
+let mongodb_uri = process.env.MONGODB_CONNECTION_STRING;
 
+//Home route for server side only for demo purpose:
 app.get("/", (req, res) => {
   res.send("server is running Sucessfully");
 });
 
-//Application listening PORT Number
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+//MongoDB Conncetion established:
+mongoose
+  .connect(mongodb_uri)
+  .then(() => {
+    console.log("MongoDb Connected Succesfully");
+    try {
+      //Application listening PORT Number
+      app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+      });
+    } catch (error) {
+      console.log("Server Connection Failure");
+    }
+  })
+  .catch((error) => {
+    console.log("MongoDb Connection Failure");
+  });
