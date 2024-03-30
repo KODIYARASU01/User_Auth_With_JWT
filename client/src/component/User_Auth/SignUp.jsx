353148fa75672e3a2663410b895d8a64b53cb326
KODIYARASU01 <kodiyarasu02gmail.com>
2024-03-30 06:56:57 +0000

    Client side signup page profile image upload alignment changed

--- client/src/component/User_Auth/SignUp.jsx
+++ client/src/component/User_Auth/SignUp.jsx
@@ -12,29 +12,29 @@ import "react-toastify/dist/ReactToastify.css";
 import formContext from "../Context/FormContext.jsx";
 
 const SignUp = () => {
-  let navigate=useNavigate();
-let {
-  show,
-  setShow,
-  profile,
-  setProfile,
-  firstName,
-  setFirstName,
-  lastName,
-  setLastName,
-  location,
-  setLocation,
-  mobileNumber,
-  setMobileNumber,
-  email,
-  setEmail,
-  password,
-  setPassword,
-  loader,
-  setLoader,
-  UserDetails,
-  setUserDetails,
-} = useContext(formContext)
+  let navigate = useNavigate();
+  let {
+    show,
+    setShow,
+    profile,
+    setProfile,
+    firstName,
+    setFirstName,
+    lastName,
+    setLastName,
+    location,
+    setLocation,
+    mobileNumber,
+    setMobileNumber,
+    email,
+    setEmail,
+    password,
+    setPassword,
+    loader,
+    setLoader,
+    UserDetails,
+    setUserDetails,
+  } = useContext(formContext);
   //Formik does not support file upload so we could create handler :
   const onUpload = async (e) => {
     let base64 = await convertToBase64(e.target.files[0]);
@@ -74,17 +74,24 @@ let {
         </div>
         <div className="box_container">
           <div className="right_form">
-           
             <div className="form_title">
-              <h4>Welcome to AristosTech Digital Card Creator!</h4>
+              <h4>Welcome to My Application!</h4>
               <p>Create your new Account</p>
-              <div className="profile">
+            </div>
+            <div className="illustration">
+              {/* <img src={illustration} alt="illustration" /> */}
+            </div>
+         
+            <form action="">
+            <div className="profile">
               <label htmlFor="profile">
                 <img
                   src={profile || profile_logo}
                   alt="avatar"
                   id="profile_image"
                 />
+                <i className='bx bxs-chevrons-left bx-flashing' ></i>
+                <span>Upload your profile</span>
               </label>
               <input
                 onChange={onUpload}
@@ -93,11 +100,6 @@ let {
                 name="profile"
               />
             </div>
-            </div>
-            <div className="illustration">
-              <img src={illustration} alt="illustration" />
-            </div>
-            <form action="">
               {/* //First Name */}
               <div className="form_group">
                 <label htmlFor="userName">
@@ -181,7 +183,7 @@ let {
                   )}
                 </div>
               </div>
-    
+
               {/* MobileNumber`` */}
               <div className="form_group">
                 <label htmlFor="lastName">Mobile Number</label>
